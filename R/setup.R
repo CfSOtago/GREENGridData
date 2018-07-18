c9a7b76444efc9def5e11b2ea0a3b8778619eaa9
Ben Anderson <dataknut@icloud.com>
2018-07-19 10:19:34 +1200

    amended package setup for new package name

--- R/setup.R
+++ R/setup.R
@@ -1,6 +1,6 @@
-#' Sets a range of parameters re-used throughout the repo by sourcing the <repo>/ggParams.R file
+#' Sets a range of parameters re-used throughout the repo by sourcing the <repo>/ggrParams.R file
 #'
-#' \code{setup} sources the <repo>/ggParams.R file which sets a range of parameters as elements of the `ggParams` list. These include data sources and .Rmd include locations.
+#' \code{setup} sources the <repo>/ggrParams.R file which sets a range of parameters as elements of the `ggrParams` list. These include default data sources and .Rmd include locations.
 #'
 #'   The parameters can be over-written in scipts if needed but do so wih care!
 #'
@@ -8,6 +8,6 @@
 #' @export
 #'
 setup <- function(){
-  pLoc <- nzGREENGridr::findParentDirectory("nzGREENGridr") # R project location
+  pLoc <- nzGREENGridDataR::findParentDirectory("nzGREENGridDataR") # R project location
   source(paste0(pLoc, "/ggrParams.R"))
 }
