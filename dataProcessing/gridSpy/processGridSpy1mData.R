a568ef1f4972b2577ca89ccd7da99e373ebb0be2
Ben Anderson <dataknut@icloud.com>
2018-07-20 12:15:55 +1200

    added dateTime checks to see if there are repeated/lost hours at DST start/end

--- dataProcessing/gridSpy/processGridSpy1mData.R
+++ dataProcessing/gridSpy/processGridSpy1mData.R
@@ -20,18 +20,20 @@ try(file.remove(gSpyParams$hhStatsByDate)) # otherwise the append within the get
 try(file.remove(gSpyParams$fLoadedStats)) # otherwise the append within the get data function will keep adding
 
 # > Get file list ----
+
 fListAllDT <- nzGREENGridDataR::getGridSpyFileList(gSpyParams$gSpyInPath, # where to look
                                                    gSpyParams$pattern, # what to look for
                                                    gSpyParams$gSpyFileThreshold # file size threshold
-                                                   )
+)
 
 # > Fix ambiguous dates in meta data derived from file listing ----
 fListAllDT <- nzGREENGridDataR::fixAmbiguousDates(fListAllDT)
 
 # > Save the full list listing ----
-ofile <- paste0(gSpyParams$fListAll) #  set in global
-print(paste0("Saving 1 minute data files interim metadata to ", ofile))
+ofile <- gSpyParams$fListAll #  set in global
+print(paste0("Saving full list of 1 minute data files with metadata to ", ofile))
 data.table::fwrite(fListAllDT, ofile)
+
 
 print(paste0("Overall we have ", nrow(fListAllDT), " files from ",
              uniqueN(fListAllDT$hhID), " households."))
@@ -49,18 +51,27 @@ print(paste0("Loading the ", nrow(fListToLoadDT),
 hhIDs <- unique(fListToLoadDT$hhID) # list of household ids
 
 for(hh in hhIDs){ # X >> start of per household loop ----
-  # > set start time ----
+  # > Set start time ----
   startTime <- proc.time()
+  # > Process files ----
   fileList <- fListToLoadDT[hhID == hh, fullPath] # files for this household
+  print("####>")
+  print(paste0(hh, ": Start"))
   dt <- processHhGridSpyData(hh, fileList) # returns final data table for testing if required
   t <- proc.time() - startTime
   print(paste0("Data for ", hh, " loaded in ", getDuration(t)))
   
+  # > Run basic tests ----
   print(paste0("Running basic tests for: ", hh))
+  # set some vars to aggregate by
   dt <- dt[, month := lubridate::month(r_dateTime, label = TRUE)]
   dt <- dt[, year := lubridate::year(r_dateTime)]
+  dt <- dt[, obsHour := lubridate::hour(r_dateTime)]
+  dt <- dt[, obsDate := lubridate::date(r_dateTime)]
   dt <- dt[, obsTime := hms::as.hms(r_dateTime)]
   
+  # >> power checks ----
+  # These will show us when the household was consuming electricity - do they look OK?
   plotDT <- dt[, .(meanW = mean(powerW)), keyby = .(circuitLabel, month, year, obsTime)
                ]
   vLineAlpha <- 0.4
@@ -88,10 +99,78 @@ for(hh in hhIDs){ # X >> start of per household loop ----
     scale_x_time(breaks = timeBreaks) +
     geom_vline(xintercept = timeBreaks, alpha = vLineAlpha, colour = vLineCol)
   
-  
   ofile <- paste0(gSpyParams$gSpyOutPath, "checkPlots/", hh, "_powerPlot.png")
   ggsave(ofile, height = 10)
   
+  # >> date time checks ----
+  # This will tell us when we have too few or too many observations
+  # We should have too many once a year when the DST change occurs and we 'gain' an hour (we have 02:00 - 03:00 twice)
+  # and the reverse once a year when we lose 02:00 - 03:00
+  # This will not be visible in UTC time, only local time
+  nCircuits <- uniqueN(dt$circuitLabel)
+  nExpectedObs <- nCircuits * 60
+  t <- dt[, .(nObs = .N), keyby = .(obsDate, obsHour)]
+  t$ratio <- t$nObs/nExpectedObs
+  myPlot <- ggplot(t, aes(x = obsDate, y = obsHour, fill = ratio)) + 
+    geom_tile() +
+    # https://ggplot2.tidyverse.org/reference/scale_brewer.html
+    # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+    scale_fill_distiller(palette = "Spectral", direction = -1) +
+    scale_x_date(date_breaks = "1 month") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)) +
+    labs(title = paste0("Date time plot: ", hh),
+         y = "Hour", 
+         caption = paste0("gridSpy data from ", min(dt$r_dateTime), 
+                          " to ", max(dt$r_dateTime),
+                          "\nobsDate = Pacific/Auckland",
+                          "\nWe expect ", nExpectedObs, " per hour given ", nCircuits, " circuits",
+                          "\nRatio = nObs/nExpectedObs so a value of 2 means we have duplicate hours (usualy at DST changes)"))
+  myPlot + 
+    annotate("text", x = as.Date("2014-04-06"),
+             y = 6,size = 4,angle = 90,
+             label = "DST end") +
+  annotate("text", x = as.Date("2014-09-28"),
+           y = 6,size = 4,angle = 90,
+             label = "DST start") +
+    annotate("text", x = as.Date("2015-04-05"),
+             y = 6,size = 4,angle = 90,
+             label = "DST end") +
+    annotate("text", x = as.Date("2015-09-27"),
+             y = 6,size = 4,angle = 90,
+             label = "DST start")
+  
+  ofile <- paste0(gSpyParams$gSpyOutPath, "checkPlots/", hh, "_timePlot.png")
+  ggsave(ofile, height = 10)
+
+  # > Set household level stats by date ----
+  statDT <- dt[, .(nObs = .N,
+                        meanPower = mean(powerW),
+                        sdPowerW = sd(powerW),
+                        minPowerW = min(powerW),
+                        maxPowerW = max(powerW),
+                        circuitLabels = toString(unique(circuitLabel)),
+                        nCircuits = uniqueN(circuitLabel)), # the actual number of columns in the whole household file with "$" in them in case of rbind "errors" caused by files with different column names
+                    keyby = .(hhID, date = lubridate::date(r_dateTime))] # can't do sensible summary stats on W as some circuits are sub-sets of others!
+  
+  # > Add fStats to end of stats file stats collector
+  ofile <- gSpyParams$hhStatsByDate 
+  print(paste0(hh, ": Adding hh stats by date list stats to ", ofile))
+  data.table::fwrite(statDT, ofile, append=TRUE) # this will only write out column names once when file is created see ?fwrite
+
+  
+  # > Save hh file ----
+  # Keep only the vars we can't easily re-create
+  keepVars <- c("hhID", "dateTime_orig", "TZ_orig", "r_dateTime", "circuitLabel", "circuitID", "powerW")
+  saveDT <- dt[, c(keepVars)]
+  ofile <- paste0(gSpyParams$gSpyOutPath, "data/", hh,"_all_1min_data.csv")
+  print(paste0(hh, ": Saving ", ofile, "..."))
+  data.table::fwrite(dt, ofile)
+    print(paste0(hh, ": Saved ", ofile, ", gzipping..."))
+  cmd <- paste0("gzip -f ", "'", path.expand(ofile), "'") # gzip it - use quotes in case of spaces in file name, expand path if needed
+  try(system(cmd)) # in case it fails - if it does there will just be .csv files (not gzipped) - e.g. under windows
+  print(paste0(hh, ": Gzipped ", ofile))
+  print(paste0(hh, ": Done"))
+  print("<####")
 } # << X end per household loop ----
 
 
