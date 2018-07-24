8c59a1395c4b7851430eb5b519fc85614957c1b6
Ben Anderson <dataknut@icloud.com>
2018-07-24 17:59:14 +1200

    local re-run to fix rf_46 & rf_47; also added more visual feedback for running from Rscript

--- dataProcessing/gridSpy/makeFile.R
+++ dataProcessing/gridSpy/makeFile.R
@@ -5,17 +5,29 @@
 # Only run this if you have substantial new data as it takes a while.
 
 # 2: Create a data processing report using the 'meta' data created by 1. 
-# Obviously if you do not re-run 1 over new data then you will get a report based on the previous data processing run.
+# Obviously if you do not re-run 1 over new data then you will get a report 
+# based on the previous data processing run.
+
+
+# Best run using Rscript to avoid RStudio lock-up: http://www.cureffi.org/2014/01/15/running-r-batch-mode-linux/
+
 ####
 
 # Housekeeping ----
 rm(list=ls(all=TRUE)) # remove all objects from workspace
 
+print(paste0("#--------------- Processing NZ GREEN Grid Grid Spy Data ---------------#"))
+
 # Load nzGREENGrid package ----
+
+print(paste0("#--- Load nzGREENGridDataR package"))
 library(nzGREENGridDataR) # local utilities
+print(paste0("#--- Done "))
 
 # Set global package parameters ----
+print(paste0("#--- Set up nzGREENGridDataR package "))
 nzGREENGridDataR::setup()
+print(paste0("#--- Done "))
 
 # Set gSpy parameters ----
 gSpyParams <- list()
@@ -23,11 +35,12 @@ gSpyParams$fullFb <- 0 # give full feedback (1 = yes)?
 gSpyParams$gSpyFileThreshold <- 3000 # assume any files smaller than this (bytes) = no data or some mangled xml/html. Really, we should check the contents of each file.
 
 gSpyParams$pattern <- "*at1.csv$" # e.g. *at1.csv$ filters only 1 min data
+gSpyParams$refreshFileList <- 1 # forces new file search & ignores any previously saved lists
 
 # Set local (this script) parameters ----
 refreshData <- 1 # 0 = No
 buildReport <- 1 # 0 = No
-localTest <- 0 # local data test or not (1 = yes)?
+localTest <- 1 # local data test or not (1 = yes)?
 
 # > Set data paths  ----
 
@@ -38,20 +51,17 @@ if(localTest){
   # Local test
   gSpyParams$gSpyInPath <- "~/Data/NZGreenGrid/gridspy/1min_orig/" # location of data (BA laptop)
   gSpyParams$gSpyOutPath <- "~/Data/NZGreenGrid/safe/gridSpy/1min/" # place to save them (BA laptop)
-  msg1 <- paste0("Test run using reduced data from ", 
+  msg1 <- paste0("#--- Test run using reduced data from ", 
                  gSpyParams$gSpyInPath, " and saving to ",
                  gSpyParams$gSpyOutPath)
 } else {
   # full monty
   gSpyParams$gSpyInPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/_RAW DATA/GridSpyData/" # location of data
   gSpyParams$gSpyOutPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/Clean_data/safe/gridSpy/1min/" # place to save them
-  msg1 <- paste0("Full run using full data from ", 
+  msg1 <- paste0("#--- Full run using full data from ", 
                  gSpyParams$gSpyInPath, " and saving to ",
                  gSpyParams$gSpyOutPath)
 }
-
-print(msg1)
-
 
 gSpyParams$fListAll <- paste0(gSpyParams$gSpyOutPath, "checkStats/fListAllDT.csv") # place to store the interim file list with initial meta-data
 gSpyParams$fLoadedStats <- paste0(gSpyParams$gSpyOutPath, "checkStats/fLoadedStats.csv") # place to store the final loaded file list with all meta-data
@@ -68,31 +78,38 @@ getDuration <- function(t){
   return(duration)
 }
 
+# --- Code ---
+
+
+print(msg1)
+
 # --- Refresh data ----
 # do this by sourcing the R code
-
-# > set start time ----
-startTime <- proc.time()
-
 if(refreshData){
-  print(paste0("Refreshing all data using ", gSpyParams$gSpyInPath))
-  source(paste0(ggrParams$projLoc, "/dataProcessing/gridSpy/processGridSpy1mData.R"))
+  # > set start time ----
+  startTime <- proc.time()
+  print(paste0("#-- Refreshing all data using ", gSpyParams$gSpyInPath))
+  sourceF <- paste0(ggrParams$projLoc, "/dataProcessing/gridSpy/processGridSpy1mData.R")
+  print(paste0("#-- Running ", sourceF))
+  source(sourceF)
+  print("#-- Data refresh complete") 
+  t <- proc.time() - startTime
+  print(paste0("#-- Data refresh completed in ", getDuration(t)))
 }
-t <- proc.time() - startTime
 
-print(paste0("Data refresh completed in ", getDuration(t)))
-
-
-# > set start time ----
-startTime <- proc.time()
 
 # --- Build report ----
+# Via (parameterised) .Rmd
 if(buildReport){
-  print("Rebuilding report")
-  #source("buildGridSpy1mReport.Rmd")
-  # Do some checks on DSTT change hours
+  startTime <- proc.time()
+  print("#-- Rebuilding report")
+  # run the report .Rmd and render to pdf
+  rmarkdown::render(input = "buildGridSpy1mReport.Rmd",
+                    output_format = "pdf_document",
+                    output_file = paste0(gSpyParams$gSpyOutPath,"processingReports/gridSpy1mProcessingReport.pdf")
+  )
+  t <- proc.time() - startTime
+  print(paste0("Report rebuild completed in ", getDuration(t)))
 }
 
-t <- proc.time() - startTime
 
-print(paste0("Report rebuild completed in ", getDuration(t)))
