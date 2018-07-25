f0ca4d4377e5bdfdc63452eaa2b8092eae1202c3
Ben Anderson <dataknut@icloud.com>
2018-07-26 09:07:42 +1200

    added main params setting from file shared by .R & .Rmd; re-ran over all data after rf_46 function fix

---
+++ dataProcessing/gridSpy/gSpyParams.R
@@ -0,0 +1,33 @@
+# Set gSpy parameters ----
+gSpyParams <- list()
+gSpyParams$fullFb <- 0 # give full feedback (1 = yes)?
+gSpyParams$refreshFileList <- 1 # forces new file search & ignores any previously saved lists
+
+gSpyParams$gSpyFileThreshold <- 3000 # assume any files smaller than this (bytes) = no data or some mangled xml/html. Really, we should check the contents of each file.
+
+gSpyParams$pattern <- "*at1.csv$" # e.g. *at1.csv$ filters only 1 min data
+
+gSpyParams$dstNZDates <- paste0(nzGREENGridDataR::findParentDirectory("nzGREENGridDataR"),"/data/dstNZDates.csv")
+gSpyParams$gSpyFileThreshold
+
+# Must be run after localData is set
+
+if(localData){
+  # Local test
+  gSpyParams$gSpyInPath <- "~/Data/NZGreenGrid/gridspy/1min_orig/" # location of data (BA laptop)
+  gSpyParams$gSpyOutPath <- "~/Data/NZGreenGrid/safe/gridSpy/1min/" # place to save them (BA laptop)
+  msg1 <- paste0("#--- Test run using reduced data from ", 
+                 gSpyParams$gSpyInPath, " and saving to ",
+                 gSpyParams$gSpyOutPath)
+} else {
+  # full monty
+  gSpyParams$gSpyInPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/_RAW DATA/GridSpyData/" # location of data
+  gSpyParams$gSpyOutPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/Clean_data/safe/gridSpy/1min/" # place to save them
+  msg1 <- paste0("#--- Full run using full data from ", 
+                 gSpyParams$gSpyInPath, " and saving to ",
+                 gSpyParams$gSpyOutPath)
+}
+
+gSpyParams$fListAll <- paste0(gSpyParams$gSpyOutPath, "checkStats/fListAllDT.csv") # place to store the interim file list with initial meta-data
+gSpyParams$fLoadedStats <- paste0(gSpyParams$gSpyOutPath, "checkStats/fLoadedStats.csv") # place to store the final loaded file list with all meta-data
+gSpyParams$hhStatsByDate <- paste0(gSpyParams$gSpyOutPath, "checkStats/hhStatsByDate.csv") # place to store the final hh summary stats
