d51f581fefb8da71f9c9c2d7323e4844876625d1
Ben Anderson <dataknut@icloud.com>
2018-07-26 14:10:32 +1200

    amended to save report to github; re-run over full data

--- dataProcessing/gridSpy/makeFile.R
+++ dataProcessing/gridSpy/makeFile.R
@@ -35,9 +35,9 @@ nzGREENGridDataR::loadLibraries(localLibs)
 
 
 # Set local (this script) parameters ----
-refreshData <- 0 # 0 = No
+refreshData <- 1 # 0 = No
 buildReport <- 1 # 0 = No
-localData <- 1 # local data test or not (1 = yes)?
+localData <- 0 # local data test or not (1 = yes)?
 
 # Set grid spy data paths etc from file ----
 source(paste0(ggrParams$projLoc, "/dataProcessing/gridSpy/gSpyParams.R"))
@@ -83,7 +83,7 @@ if(buildReport){
   rmarkdown::render(input = rmdFile,
                     output_format = "html_document",
                     params = list(localData = localData),
-                    output_file = paste0(gSpyParams$gSpyOutPath,"processingReports/gridSpy1mProcessingReport.html")
+                    output_file = paste0(ggrParams$projLoc,"/dataProcessing/gridSpy/gridSpy1mProcessingReport.html")
   )
   t <- proc.time() - startTime
   print(paste0("Report rebuild completed in ", getDuration(t)))
