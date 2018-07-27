5b8a6dd3bf4fb58691d444b74444d8313591b248
Ben Anderson <dataknut@icloud.com>
2018-07-27 12:00:40 +1200

    amended .Rmd text; changed location for saving plots; changed location for saving report

--- dataProcessing/gridSpy/makeFile.R
+++ dataProcessing/gridSpy/makeFile.R
@@ -37,7 +37,7 @@ nzGREENGridDataR::loadLibraries(localLibs)
 # Set local (this script) parameters ----
 refreshData <- 0 # 0 = No
 buildReport <- 1 # 0 = No
-localData <- 0 # local data test or not (1 = yes)?
+localData <- 1 # local data test or not (1 = yes)?
 
 # Set grid spy data paths etc from file ----
 source(paste0(ggrParams$projLoc, "/dataProcessing/gridSpy/gSpyParams.R"))
@@ -83,7 +83,7 @@ if(buildReport){
   rmarkdown::render(input = rmdFile,
                     output_format = "html_document",
                     params = list(localData = localData),
-                    output_file = paste0(ggrParams$projLoc,"/dataProcessing/gridSpy/gridSpy1mProcessingReport.html")
+                    output_file = paste0(ggrParams$projLoc,"/reports/gridSpy1mProcessingReport.html")
   )
   t <- proc.time() - startTime
   print(paste0("Report rebuild completed in ", getDuration(t)))
