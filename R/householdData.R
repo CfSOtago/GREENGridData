de3b5da73ef78f70359336f9829c50e051b054dc
Ben Anderson <dataknut@icloud.com>
2018-07-27 15:14:35 +1200

    updated household attributes creation code & report; added TUD stub; re-built docs; added/edited ggrParams

--- R/householdData.R
+++ R/householdData.R
@@ -1,31 +1,48 @@
 #' Loads gridSpy household data from .xlsx
 #'
-#' \code{getHouseholdData} does what it says. Expects .xlsx with two sheets, one for each sample.
+#' \code{getHouseholdData} does what it says. Expects an .xlsx with two sheets, one for each sample.
 #'
-#' Combines the results into one table. Could be extended to include other survey data when available.
+#' Combines & returns the results as a data.table which records:
+#' 
+#'    - power company
+#'    - location
+#'    - n adults
+#'    - n children aged 0-12
+#'    - n chidlren aged 13-18
+#'    - any recruitment or sampling notes
+#'    - date gridSpy monitor disconnected and (potentially) re-used
+#'    
+#'    Will be extended to include other survey data when available.
 #'
 #' @param f the .xlsx file
 #'
 #' @import data.table
+#' @import lubridate
+#' @import readxl
 #'
 #' @author Ben Anderson, \email{b.anderson@@soton.ac.uk}
 #' @export
 #'
 #'
 getHouseholdData <- function(f) {
+  #f <- ggrParams$gsHHMasterFile
   unisonDT <- data.table::as.data.table(readxl::read_xlsx(f, sheet = "Unison"))
 
   # keep safe data only
-  unisonDT <- unisonDT[, .(sample = `Power company`, hhID = `Tag`, newID,
-                           Location, nAdults, nChildren0_12, nTeenagers13_18, outlierFlag,removed )]
+  unisonDT <- unisonDT[, .(sample = `Power company`, hhID, newID,
+                           Location, nAdults, nChildren0_12, nTeenagers13_18, notes, stopDate)]
+  unisonDT <- unisonDT[, r_stopDate := lubridate::ymd(stopDate)]
+  unisonDT$stopDate <- NULL
   #head(unisonDT)
-
+  
   powercoDT <- data.table::as.data.table(readxl::read_xlsx(f, sheet = "Powerco"))
-  powercoDT <- powercoDT[, .(sample = `Power Co`, hhID = `Building Tag`, newID,
-                             Location,nAdults,nChildren0_12,nTeenagers13_18,outlierFlag, removed = `date disconnected` )]
+  powercoDT <- powercoDT[, .(sample = `Power Co`, hhID, newID,
+                             Location,nAdults,nChildren0_12,nTeenagers13_18,notes, stopDate)]
+  powercoDT <- powercoDT[, r_stopDate := lubridate::ymd(stopDate)]
+  powercoDT$stopDate <- NULL
   #head(powercoDT)
 
   # remove NA on rbind
-  dt <- rbind(unisonDT, powercoDT[!is.na(hhID)])
+  dt <- rbind(unisonDT[!is.na(hhID)], powercoDT[!is.na(hhID)])
   return(dt)
 }
