b87839def74cb2c7c0aba35f3911df85dc52476e
Ben Anderson <dataknut@icloud.com>
2018-08-02 09:00:02 +1200

    updated location of gs file stats

--- ggrParams.R
+++ ggrParams.R
@@ -7,6 +7,13 @@ ggrParams$dataLoc <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/" # HPS by
 # Location of basic household data
 ggrParams$gsHHMasterFile <- "~/Syncplicity Folders/Green Grid Project Management Folder/Gridspy/Master list of Gridspy units.xlsx"
 
+# Location of stats
+ggrParams$statsLoc <- paste0(ggrParams$dataLoc, "Clean_data/safe/gridSpy/1min/checkStats/")
+
+# Location of safe data
+ggrParams$gsSafe <- paste0(ggrParams$dataLoc, "Clean_data/safe/gridSpy/1min/data/")
+ggrParams$hhAttributes <- paste0(ggrParams$dataLoc, "Clean_data/safe/survey/ggHouseholdAttributes.csv")
+
 # Vars for Rmd
 ggrParams$pubLoc <- "[Centre for Sustainability](http://www.otago.ac.nz/centre-sustainability/), University of Otago: Dunedin"
 ggrParams$otagoHCS <- "the University of Otago's High-Capacity Central File Storage [HCS](https://www.otago.ac.nz/its/services/hosting/otago068353.html)"
