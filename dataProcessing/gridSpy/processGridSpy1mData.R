17c4d0f7abe31537066f6ebffadca8b71581c63f
Ben Anderson <dataknut@icloud.com>
2018-08-02 14:32:33 +1200

    added functions to make plots & save data to more easily deal with data split by newID as a result of kit re-use

--- dataProcessing/gridSpy/processGridSpy1mData.R
+++ dataProcessing/gridSpy/processGridSpy1mData.R
@@ -12,6 +12,98 @@ localLibs <- c("data.table",
 nzGREENGridDataR::loadLibraries(localLibs)
 
 # Local functions ----
+makePowerPlot  <- function(hh,dt){
+  # make plot of power by month, year & circuit
+  # These will show us when the household was consuming electricity 
+  # - do they look OK?
+  # - can you spot the households with PV?
+  
+  plotDT <- dt[, .(meanW = mean(powerW)), keyby = .(circuit, month, year, obsTime)
+               ] # aggregate by circuit to preserve unique circuit labels in households 
+  # (e.g. rf_46) where names are re-used but with different ids. see ?fixCircuitLabels_rf_46
+  vLineAlpha <- 0.4
+  vLineCol <- "#0072B2" # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#a-colorblind-friendly-palette
+  timeBreaks <- c(hms::as.hms("02:00:00"), hms::as.hms("04:00:00"), 
+                  hms::as.hms("06:00:00"), hms::as.hms("08:00:00"),
+                  hms::as.hms("10:00:00"), hms::as.hms("12:00:00"),
+                  hms::as.hms("14:00:00"), hms::as.hms("16:00:00"),
+                  hms::as.hms("18:00:00"), hms::as.hms("20:00:00"),
+                  hms::as.hms("22:00:00")
+  )
+  myPlot <- ggplot2::ggplot(plotDT, aes(x = obsTime, y = meanW/1000, colour = circuit)) +
+    geom_line() + 
+    facet_grid(month  ~ year) + 
+    theme(strip.text.y = element_text(angle = 0, vjust = 0.5, hjust = 0.5)) + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)) + 
+    theme(legend.position = "bottom") + 
+    labs(title = paste0("Montly mean power profiles by circuit plot: ", hh),
+         y = "Mean kW", 
+         caption = paste0("gridSpy data from ", min(dt$r_dateTime), 
+                          " to ", max(dt$r_dateTime),
+                          "\nobsTime = Pacific/Auckland"))
+  
+  myPlot + 
+    scale_x_time(breaks = timeBreaks) +
+    geom_vline(xintercept = timeBreaks, alpha = vLineAlpha, colour = vLineCol)
+  
+  ofile <- paste0(plotLoc, hh, "_monthlyPowerPlot.png")
+  ggplot2::ggsave(ofile, height = 10)
+}
+
+makeObsPlot  <- function(hh,dt){
+  # plots n obs per hour & date
+  # This will tell us when we have too few or too many observations
+  # We should have too many once a year when the DST change occurs and we 'gain' an hour (we have 02:00 - 03:00 twice)
+  # and the reverse once a year when we lose 02:00 - 03:00
+  # This will not be visible in UTC time, only local time
+  nCircuits <- uniqueN(dt$circuit)
+  nExpectedObs <- nCircuits * 60
+  t <- dt[, .(nObs = .N), keyby = .(obsDate, obsHour)]
+  t$ratio <- t$nObs/nExpectedObs
+  myPlot <- ggplot2::ggplot(t, aes(x = obsDate, y = obsHour, fill = ratio)) + 
+    geom_tile() +
+    # https://ggplot2.tidyverse.org/reference/scale_brewer.html
+    # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+    #scale_fill_distiller(palette = "Spectral", direction = -1) +
+    scale_fill_gradient2(midpoint = 1) +
+    scale_x_date(date_breaks = "2 months") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)) +
+    labs(title = paste0("Expected observations ratio by date & hour plot: ", hh),
+         y = "Hour", 
+         caption = paste0("gridSpy data from ", min(dt$r_dateTime), 
+                          " to ", max(dt$r_dateTime),
+                          "\nobsDate = Pacific/Auckland",
+                          "\nWe expect ", nExpectedObs, " per hour given ", nCircuits, " circuits",
+                          "\nRatio = nObs/nExpectedObs so a value of 2 means we have duplicate hours (usualy at DST changes)"))
+  
+  # Add DST labels. We add all labels as it helps to show which households have ongoing data collection
+  for(d in unique(dstDT$date)){
+    obsDate <- lubridate::dmy(d)
+    label <- dstDT[date == d, label]
+    myPlot <- myPlot +
+      annotate("text", x = obsDate,
+               y = 7,size = 3, angle = 90,
+               label = label)
+  }
+  myPlot 
+  
+  ofile <- paste0(plotLoc, hh, "_observationsRatioPlot.png")
+  ggplot2::ggsave(ofile, height = 10)
+}
+
+saveFinalDT <- function(hh,dt){
+  # Keep only the vars we can't easily re-create
+  keepVars <- c("hhID", "newID", "dateTime_orig", "TZ_orig", "r_dateTime", "circuit", "powerW")
+  saveDT <- dt[, ..keepVars] # just saves the keepVars
+  ofile <- paste0(gSpyParams$gSpyOutPath, "data/", hh,"_all_1min_data.csv")
+  print(paste0("#--> ", hh, ": Saving ", ofile, " (columns: ", toString(names(saveDT)), ")"))
+  data.table::fwrite(saveDT, ofile)
+  print(paste0("#--> ",hh, ": Saved ", ofile, ", gzipping..."))
+  cmd <- paste0("gzip -f ", "'", path.expand(ofile), "'") # gzip it - use quotes in case of spaces in file name, expand path if needed
+  try(system(cmd)) # in case it fails - if it does there will just be .csv files (not gzipped) - e.g. under windows
+  print(paste0("#--> ",hh, ": Gzipped ", ofile))
+  print(paste0("#--> ",hh, ": Done"))
+}
 
 # Local parameters ----
 
@@ -108,110 +200,62 @@ for(hh in hhIDs){ # X >> start of per household loop ----
   dt <- dt[, obsHour := lubridate::hour(r_dateTime)]
   dt <- dt[, obsDate := lubridate::date(r_dateTime)]
   dt <- dt[, obsTime := hms::as.hms(r_dateTime)]
-  
-  # >> power checks ----
-  # These will show us when the household was consuming electricity 
-  # - do they look OK?
-  # - can you spot the households with PV?
-  
-  plotDT <- dt[, .(meanW = mean(powerW)), keyby = .(circuit, month, year, obsTime)
-               ] # aggregate by circuit to preserve unique circuit labels in households 
-                 # (e.g. rf_46) where names are re-used but with different ids. see ?fixCircuitLabels_rf_46
-  vLineAlpha <- 0.4
-  vLineCol <- "#0072B2" # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#a-colorblind-friendly-palette
-  timeBreaks <- c(hms::as.hms("02:00:00"), hms::as.hms("04:00:00"), 
-                  hms::as.hms("06:00:00"), hms::as.hms("08:00:00"),
-                  hms::as.hms("10:00:00"), hms::as.hms("12:00:00"),
-                  hms::as.hms("14:00:00"), hms::as.hms("16:00:00"),
-                  hms::as.hms("18:00:00"), hms::as.hms("20:00:00"),
-                  hms::as.hms("22:00:00")
-  )
-  myPlot <- ggplot2::ggplot(plotDT, aes(x = obsTime, y = meanW/1000, colour = circuit)) +
-    geom_line() + 
-    facet_grid(month  ~ year) + 
-    theme(strip.text.y = element_text(angle = 0, vjust = 0.5, hjust = 0.5)) + 
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)) + 
-    theme(legend.position = "bottom") + 
-    labs(title = paste0("Montly mean power profiles by circuit plot: ", hh),
-         y = "Mean kW", 
-         caption = paste0("gridSpy data from ", min(dt$r_dateTime), 
-                          " to ", max(dt$r_dateTime),
-                          "\nobsTime = Pacific/Auckland"))
-    
-  myPlot + 
-    scale_x_time(breaks = timeBreaks) +
-    geom_vline(xintercept = timeBreaks, alpha = vLineAlpha, colour = vLineCol)
-  
-  ofile <- paste0(plotLoc, hh, "_monthlyPowerPlot.png")
-  ggsave(ofile, height = 10)
-  
-  # >> date time checks ----
-  # This will tell us when we have too few or too many observations
-  # We should have too many once a year when the DST change occurs and we 'gain' an hour (we have 02:00 - 03:00 twice)
-  # and the reverse once a year when we lose 02:00 - 03:00
-  # This will not be visible in UTC time, only local time
-  nCircuits <- uniqueN(dt$circuit)
-  nExpectedObs <- nCircuits * 60
-  t <- dt[, .(nObs = .N), keyby = .(obsDate, obsHour)]
-  t$ratio <- t$nObs/nExpectedObs
-  myPlot <- ggplot2::ggplot(t, aes(x = obsDate, y = obsHour, fill = ratio)) + 
-    geom_tile() +
-    # https://ggplot2.tidyverse.org/reference/scale_brewer.html
-    # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
-    #scale_fill_distiller(palette = "Spectral", direction = -1) +
-    scale_fill_gradient2(midpoint = 1) +
-    scale_x_date(date_breaks = "2 months") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)) +
-    labs(title = paste0("Expected observations ratio by date & hour plot: ", hh),
-         y = "Hour", 
-         caption = paste0("gridSpy data from ", min(dt$r_dateTime), 
-                          " to ", max(dt$r_dateTime),
-                          "\nobsDate = Pacific/Auckland",
-                          "\nWe expect ", nExpectedObs, " per hour given ", nCircuits, " circuits",
-                          "\nRatio = nObs/nExpectedObs so a value of 2 means we have duplicate hours (usualy at DST changes)"))
-  
-  # Add DST labels. We add all labels as it helps to show which households have ongoing data collection
-  for(d in unique(dstDT$date)){
-    obsDate <- lubridate::dmy(d)
-    label <- dstDT[date == d, label]
-    myPlot <- myPlot +
-      annotate("text", x = obsDate,
-               y = 7,size = 3, angle = 90,
-               label = label)
-  }
-  myPlot 
-  
-  ofile <- paste0(plotLoc, hh, "_observationsRatioPlot.png")
-  ggsave(ofile, height = 10)
 
+  # > Fix the re-used rf_XX before we save anything ----
+  if(hh == "rf_15"){
+    print("#---> fixing rf_15 re-use")
+    # rf_15a up to 2015-01-15 then rf_15b to 2016-04-02 # see https://dataknut.github.io/nzGREENGridDataR/surveyProcessingReport.html
+    saveDT <- saveDT[, linkID := "rf_15a"]
+    saveDT <- saveDT[lubridate::date(r_dateTime) > as.Date("2015-01-15"), linkID := "rf_15b"]
+    # saveDT[, .(nObs = .N, 
+    #            minDate = min(as.Date(r_dateTime)),
+    #            maxDate = max(as.Date(r_dateTime))), keyby = .(linkID)]
+    dta <- saveDT[linkID == "rf_15a"]
+    hh <- "rf_15a"
+    makePowerPlot(hh,dta)
+    makeObsPlot(hh,dta)
+    saveFinalDT(hh,dta)
+    hh <- "rf_15b"
+    dtb <- saveDT[linkID == "rf_15b"]
+    makePowerPlot(hh,dtb)
+    makeObsPlot(hh,dtb)
+    saveFinalDT(hh,dtb)
+  } else if (hh == "rf_17"){
+    print("#---> fixing rf_17 re-use")
+    # rf_17a up to 2016-03-28 then rf_17b
+    saveDT <- saveDT[, linkID := "rf_17a"]
+    saveDT <- saveDT[lubridate::date(r_dateTime) > as.Date("2016-03-28"), linkID = "rf_17b"]
+    hh <- "rf_17a"
+    dta <- saveDT[linkID == hh]
+    makePowerPlot(hh,dta)
+    makeObsPlot(hh,dta)
+    saveFinalDT(hh,dta)
+    hh <- "rf_17b"
+    dtb <- saveDT[linkID == hh]
+    makePowerPlot(hh,dtb)
+    makeObsPlot(hh,dtb)
+    saveFinalDT(hh,dtb)
+  } else {
+    # no problems so set newID to hhID
+    saveDT <- saveDT[, linkID := hhID]
+    saveFinalDT(hh,saveDT)
+  }
+  
   # > Set household level stats by date ----
-  statDT <- dt[, .(nObs = .N,
-                        meanPower = mean(powerW),
-                        sdPowerW = sd(powerW),
-                        minPowerW = min(powerW),
-                        maxPowerW = max(powerW),
-                        circuitLabels = toString(unique(circuit)),
-                        nCircuits = uniqueN(circuit)), # the actual number of columns in the whole household file with "$" in them in case of rbind "errors" caused by files with different column names
-                    keyby = .(hhID, date = lubridate::date(r_dateTime))] # can't do sensible summary stats on W as some circuits are sub-sets of others!
+  statDT <- saveDT[, .(nObs = .N,
+                   meanPower = mean(powerW),
+                   sdPowerW = sd(powerW),
+                   minPowerW = min(powerW),
+                   maxPowerW = max(powerW),
+                   circuitLabels = toString(unique(circuit)),
+                   nCircuits = uniqueN(circuit)), # the actual number of columns in the whole household file with "$" in them in case of rbind "errors" caused by files with different column names
+               keyby = .(linkID, date = lubridate::date(r_dateTime))] # can't do sensible summary stats on W as some circuits are sub-sets of others!
   
   # > Add fStats to end of stats file stats collector
   ofile <- gSpyParams$hhStatsByDate 
   print(paste0("#--> ",hh, ": Adding hh stats by date list stats to ", ofile))
   data.table::fwrite(statDT, ofile, append=TRUE) # this will only write out column names once when file is created see ?fwrite
-
   
-  # > Save hh file ----
-  # Keep only the vars we can't easily re-create
-  keepVars <- c("hhID", "dateTime_orig", "TZ_orig", "r_dateTime", "circuit", "powerW")
-  saveDT <- dt[, ..keepVars] # just saves the keepVars
-  ofile <- paste0(gSpyParams$gSpyOutPath, "data/", hh,"_all_1min_data.csv")
-  print(paste0("#--> ", hh, ": Saving ", ofile, " (columns: ", toString(names(saveDT)), ")"))
-  data.table::fwrite(saveDT, ofile)
-  print(paste0("#--> ",hh, ": Saved ", ofile, ", gzipping..."))
-  cmd <- paste0("gzip -f ", "'", path.expand(ofile), "'") # gzip it - use quotes in case of spaces in file name, expand path if needed
-  try(system(cmd)) # in case it fails - if it does there will just be .csv files (not gzipped) - e.g. under windows
-  print(paste0("#--> ",hh, ": Gzipped ", ofile))
-  print(paste0("#--> ",hh, ": Done"))
 } # << X end per household loop ----
 
 # End
