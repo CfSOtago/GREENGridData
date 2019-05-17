df555f242184fee264bf3580c7836fdb96945bf5
Ben Anderson <dataknut@icloud.com>
2019-05-17 01:09:16 +0000

    latest run of total power demand imputation code

--- examples/code/imputeTotalPower.r
+++ examples/code/imputeTotalPower.r
@@ -34,9 +34,8 @@ sysname <- Sys.info()[[1]]
 user <- Sys.info()[[6]]
 message("Running on ", sysname, " under user ", user)
 
-#circuitsFile <- "circuitsToSum.csv"
-circuitsFile <- "circuitsToSum_v1.0" # JKM original
-#circuitsFile <- "circuitsToSum_v1.1" # all
+#circuitsFile <- "circuitsToSum_v1.0" # JKM original
+circuitsFile <- "circuitsToSum_v1.1" # all
 
 # localise data paths
 if(user == "ben" & sysname == "Darwin"){
