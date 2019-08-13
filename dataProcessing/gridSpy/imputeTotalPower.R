c5453176de987167438beaf592dad99569351662
Ben Anderson <dataknut@gridcrawler.otago.ac.nz>
2019-08-13 05:20:07 +0000

    fixed to run properly & smoothly; keeps original data format so output can be loaded using the clea data load functions

--- dataProcessing/gridSpy/imputeTotalPower.R
+++ dataProcessing/gridSpy/imputeTotalPower.R
@@ -35,23 +35,32 @@ library(here)
 # parameterise data path - edit for your set up ----
 
 sysname <- Sys.info()[[1]]
-user <- Sys.info()[[6]]
-message("Running on ", sysname, " under user ", user)
+login <- Sys.info()[[6]]
+user <- Sys.info()[[7]]
 
 # Select your circuits-to-sum file
 #circuitsFile <- "circuitsToSum_v1.0" # JKM original
 circuitsFile <- "circuitsToSum_v1.1" # all
 
 # localise data paths - aedit for your context
+DATA_PATH = "/Users/jkmair/GreenGrid/data/clean_raw" # <- Jason
+circuits_path <- paste0(DATA_PATH, "/", circuitsFile, ".csv") # <- Jason
+#circuits_path <- sprintf("%s/circuitsToSum.csv", DATA_PATH) # <- Jason
+
+if(user == "dataknut" & sysname == "Linux"){
+  # CS RStudio server
+  DATA_PATH <- "~/greenGridData/cleanData/safe/gridSpy/1min/data"
+  circuits_path <- paste0(here::here(), "/data/", circuitsFile, ".csv") # in the package data folder
+}
 if(user == "ben" & sysname == "Darwin"){
   # Ben's laptop
   DATA_PATH <- "~/Data/NZ_GREENGrid/safe/gridSpy/1min/data"
   circuits_path <- paste0(here::here(), "/data/", circuitsFile, ".csv") # in the package data folder
-} else {
-  DATA_PATH = "/Users/jkmair/GreenGrid/data/clean_raw" # <- Jason
-  circuits_path <- paste0(DATA_PATH, "/", circuitsFile, ".csv") # <- Jason
-  #circuits_path <- sprintf("%s/circuitsToSum.csv", DATA_PATH) # <- Jason
 }
+
+message("Running on ", sysname, " under user ", user)
+message("Loading data from ", DATA_PATH)
+message("Using ", circuitsFile)
 
 # set up ----
 
@@ -62,10 +71,13 @@ options(scipen=999)
 Sys.setenv(TZ='UTC')
 
 # Set the time period I want to get the per-house totals for ----
-start_time <- as.POSIXct("2015-04-01 00:00", tz="Pacific/Auckland")
-end_time <- as.POSIXct("2016-04-01 00:00", tz="Pacific/Auckland")
+start_time <- as.POSIXct("2010-01-01 00:00", tz="Pacific/Auckland")
+end_time <- as.POSIXct("2020-01-01 00:00", tz="Pacific/Auckland")
 
 start_exec <- Sys.time()
+
+message("Loading circuits from: ", circuits_path)
+circuitsDF <- read.csv(circuits_path, header = TRUE) # if we switch to readr::read_csv the processing code breaks
 
 dataL <- data.table() # data bucket to collect data in
 
@@ -73,7 +85,7 @@ dataL <- data.table() # data bucket to collect data in
 processPowerFiles <- function(df){
   message("Using data from: ", DATA_PATH)
   for(house_id in colnames(df)){
-    #house_id <- "rf_44" # for testing
+    #house_id <- "rf_19" # for testing
     
     # input <- data.table::as.data.table(readr::read_csv(sprintf("%s/%s_all_1min_data.csv.gz", DATA_PATH, house_id), 
     #                                                    col_types=cols(hhID=col_character(), 
@@ -87,20 +99,21 @@ processPowerFiles <- function(df){
     #                                    )
     iF <- sprintf("%s/%s_all_1min_data.csv.gz", DATA_PATH, house_id)
     message(" -> Loading data for ", house_id, " from ", iF)
-    inputDT <- data.table::fread(iF) # load the household data
-    
+    inputDT <- data.table::as.data.table(readr::read_csv(iF)) # load the household data
+    message("N rows: ", nrow(inputDT))
     message(" -> Parsing dates ", house_id)
-    # input$time_utc <- parse_date_time(input$r_dateTime, orders=c('ymdHMS'))
-    inputDT <- inputDT[, dateTime_utc := lubridate::as_datetime(r_dateTime)]
-    inputDT <- inputDT[, time_nz := lubridate::with_tz(dateTime_utc, tzone = "Pacific/Auckland")]
+    
+    inputDT <- inputDT[, dateTime_nz := lubridate::with_tz(r_dateTime, # stored as UTC 
+                                                       tzone = "Pacific/Auckland")] # so we can extract within NZ dateTime
     
     # select dates we want
-    inputDT <- inputDT[time_nz >= start_time & time_nz < end_time, ]
+    inputDT <- inputDT[dateTime_nz >= start_time & dateTime_nz < end_time, ]
+    message("N rows after date extraction: ", nrow(inputDT))
     
-    # Read in the circuits and extract the circuit code number
-    circuit_list <- df[, house_id]
-    circuit_list <- sub(".*\\$", "", circuit_list)
-    circuit_list <- circuit_list[circuit_list != ""]
+    # Extract the circuit code numbers
+    circuit_list <- circuitsDF[, house_id] # extracts the columns that match the house id
+    circuit_list <- sub(".*\\$", "", circuit_list) # extracts the circuit numeric label (not the string)
+    circuit_list <- circuit_list[circuit_list != ""] # makes sure no empty strings
   
     # Create a boolean array of rows/circuits to be extracted
     cond <- rep(FALSE, length(inputDT$circuit))
@@ -110,47 +123,32 @@ processPowerFiles <- function(df){
       cond <- cond | grepl(circuit, inputDT$circuit)
     }
     
-    exDT <- inputDT[cond, c("linkID", "time_nz","dateTime_utc", "powerW")]
+    circuitsToSumDT <- inputDT[cond, 
+                               c("hhID","linkID", "dateTime_orig","TZ_orig","r_dateTime", "powerW")] # uses cond to pull out just the circuits we're going to add up
       
     # make long verion (easier for data analysis)
-    totDTl <- exDT[,.(powerW = sum(powerW)), keyby = .(dateTime_utc,linkID)]
-    totDTl$circuit <- paste0("imputedTotalDemand_", circuitsFile)
+    totDTl <- circuitsToSumDT[,.(powerW = sum(powerW)), 
+                              keyby = .(hhID,linkID,dateTime_orig,TZ_orig,r_dateTime)] # so it has the same format as the original
+    totDTl$circuit <- paste0("imputedTotalDemand_", circuitsFile) # add a label so we know what it is
     # add the total back to the input DT and save as new file
-    inputDT$time_nz <- NULL # not needed
-    outDT <- rbind(inputDT[, .(linkID, circuit, dateTime_utc, powerW)]) # add total to input
+    inputDT$dateTime_nz <- NULL # not needed
+    setcolorder(inputDT, c("hhID", "linkID", "dateTime_orig", "TZ_orig", "r_dateTime", "circuit", "powerW")) # make sure they match
+    setcolorder(totDTl, c("hhID", "linkID", "dateTime_orig", "TZ_orig", "r_dateTime", "circuit", "powerW")) # make sure they match
+    outDT <- rbind(inputDT,
+                   totDTl) # add total to input
     oF <- paste0(DATA_PATH, "/imputed/",house_id, "_all_1min_data_withImputedTotal_",
-                 circuitsFile, ".csv")
+                 circuitsFile, ".csv") # use the circuitsFile name so we know the definition of which circuits we summed
     message("Saving and gzipping: ", oF)
-    data.table::fwrite(outDT, oF) # save the household data
+    data.table::fwrite(outDT, oF) # save the household data - use fwrite as it is FAST
     # gzip it
     cmd <- paste0("gzip -f ", oF)
-    try(system(cmd))
+    try(system(cmd)) # produces a warning on CS RStudio server but still works
     message("Done")
-    # remove redundant vars to save mem
-    dt <- totDTl[, .(linkID, dateTime_utc, powerW)]
-    setnames(dt, "powerW", "imputedTotalW")
-    dataL <- rbind(dataL, dt) # the big data bucket of totals
   }
   message("All households processed using: ", circuitsFile)
-  return(dataL)
 }
 
-message("Loading circuits from: ", circuits_path)
-df <- read.csv(circuits_path, header = TRUE) # if we switch to readr::read_csv the processing code breaks
-
-message("Processing")
-dataL <- processPowerFiles(df)
-
-# save out the files of all totals ----
-
-#write.table(data, file=sprintf("%s/tot.csv", DATA_PATH), sep=",", row.names=FALSE, col.names=TRUE)
-gsFile <- paste0(DATA_PATH, "/imputed/allHouseholds_totalW_long_", circuitsFile, ".csv")
-message("Saving all hosuehold data to: ", gsFile)
-data.table::fwrite(dataL, gsFile) # way faster
-
-# gzip it
-cmd <- paste0("gzip -f ", gsFile)
-try(system(cmd))
+processPowerFiles(circuitsDF)
 
 message("Done")
 
