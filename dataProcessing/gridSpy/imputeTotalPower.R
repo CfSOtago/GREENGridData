3c32d00500c635e103a7ff517294018034908e74
Ben Anderson <dataknut@icloud.com>
2019-08-15 02:19:24 +0000

    fixed to re-make the single 'all households' per-minute per-household load file

--- dataProcessing/gridSpy/imputeTotalPower.R
+++ dataProcessing/gridSpy/imputeTotalPower.R
@@ -79,7 +79,7 @@ start_exec <- Sys.time()
 message("Loading circuits from: ", circuits_path)
 circuitsDF <- read.csv(circuits_path, header = TRUE) # if we switch to readr::read_csv the processing code breaks
 
-dataL <- data.table() # data bucket to collect data in
+dataL <- data.table() # data bucket to collect per-household per-minute imputed load
 
 # process household files ----
 processPowerFiles <- function(df){
@@ -143,14 +143,21 @@ processPowerFiles <- function(df){
     # gzip it
     cmd <- paste0("gzip -f ", oF)
     try(system(cmd)) # produces a warning on CS RStudio server but still works
+    dataL <- rbind(totDTl, dataL)
     message("Done")
   }
   message("All households processed using: ", circuitsFile)
+  ofile <- paste0(DATA_PATH, "/imputed/all_1min_data_withImputedTotal_",
+                  circuitsFile, ".csv") # use the circuitsFile name so we know the definition of which circuits we summed
+  message("Saving and gzipping: ", ofile)
+  data.table::fwrite(dataL, ofile) # save the household data - use fwrite as it is FAST
+  # gzip it
+  cmd <- paste0("gzip -f ", ofile)
+  try(system(cmd)) # produces a warning on CS RStudio server but still works
+  message("Saving single file of imputed load only to: ", ofile)
 }
 
 processPowerFiles(circuitsDF)
-
-message("Done")
 
 # how long did it take?
 end_exec <- Sys.time()
