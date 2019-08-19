bdbf80bd944922945447b380de2ecd76a3cc0c61
Ben Anderson <dataknut@icloud.com>
2019-08-19 18:58:04 +1200

    fixed to use gzipped source files and to update more detailed notes from master file

--- makeDocs/makeFile.R
+++ makeDocs/makeFile.R
@@ -34,42 +34,64 @@ title <- paste0("NZ GREEN Grid Household Electricity Demand Study")
 # --- Code ---
 
 # --- Overview report ----
-subtitle <- paste0("Research data overview (version ", version, ")")
-rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildOverviewReport.Rmd")
-rmarkdown::render(input = rmdFile,
-                  output_format = "html_document2",
-                  params = list(title = title, subtitle = subtitle, version = version),
-                  output_file = paste0(ggrParams$repoLoc,"/docs/overviewReport_v", version, ".html")
-)
+doOverviewReport < function(){
+  subtitle <- paste0("Research data overview (version ", version, ")")
+  rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildOverviewReport.Rmd")
+  rmarkdown::render(input = rmdFile,
+                    output_format = "html_document2",
+                    params = list(title = title, subtitle = subtitle, version = version),
+                    output_file = paste0(ggrParams$repoLoc,"/docs/overviewReport_v", version, ".html")
+  )
+}
+
 
 # --- Survey data report ----
-subtitle <- paste0("Household attributes (version ", version, ")")
-rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildHouseholdAttributesReport.Rmd")
-rmarkdown::render(input = rmdFile,
-                  output_format = "html_document2",
-                  params = list(title = title, subtitle = subtitle, version = version),
-                  output_file = paste0(ggrParams$repoLoc,"/docs/householdAttributeProcessingReport_v", version, ".html")
-)
+doSurveyReport <- function(){
+  subtitle <- paste0("Household attributes (version ", version, ")")
+  rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildHouseholdAttributesReport.Rmd")
+  rmarkdown::render(input = rmdFile,
+                    params = list(title = title, subtitle = subtitle, version = version),
+                    output_file = paste0(ggrParams$repoLoc,"/docs/householdAttributeProcessingReport_v", version, ".html")
+  )
+}
+
 
 # --- Grid spy data report ----
 # only run this if you have to - takes a while
 localData <- 0 # 1 = yes for testing
-subtitle <- paste0("1 minute electricity power (version ", version, ")")
-rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildGridSpy1mReport.Rmd")
-rmarkdown::render(input = rmdFile,
-                  output_format = "html_document2",
-                  params = list(localData = localData, title = title, subtitle = subtitle, version = version),
-                  output_file = paste0(ggrParams$repoLoc,"/docs/gridSpy1mProcessingReport_v", version, ".html")
-)
+doGridSpyreport <- function(){
+  subtitle <- paste0("1 minute electricity power (version ", version, ")")
+  rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildGridSpy1mReport.Rmd")
+  rmarkdown::render(input = rmdFile,
+                    output_format = "html_document2",
+                    params = list(localData = localData, title = title, subtitle = subtitle, version = version),
+                    output_file = paste0(ggrParams$repoLoc,"/docs/gridSpy1mProcessingReport_v", version, ".html")
+  )
+}
+
 
 # --- Grid spy circuit level outliers report ----
 localData <- 0 # 1 = yes
-subtitle <- paste0("1 minute circuit level electricity power outlier report (version ", version, ")")
-rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildGridSpyOutliers.Rmd")
-rmarkdown::render(input = rmdFile,
-                  output_format = "html_document2",
-                  params = list(localData = localData, title = title, subtitle = subtitle, version = version),
-                  output_file = paste0(ggrParams$repoLoc,"/docs/gridSpy1mOutliersReport_v", version, ".html")
-)
+doGridSpyOutliersReport <- function(){
+  subtitle <- paste0("1 minute circuit level electricity power outlier report (version ", version, ")")
+  rmdFile <- paste0(ggrParams$repoLoc, "/makeDocs/buildGridSpyOutliers.Rmd")
+  rmarkdown::render(input = rmdFile,
+                    output_format = "html_document2",
+                    params = list(localData = localData, title = title, subtitle = subtitle, version = version),
+                    output_file = paste0(ggrParams$repoLoc,"/docs/gridSpy1mOutliersReport_v", version, ".html")
+  )
+}
+
+# --- Grid spy imputed whole household demand report ----
+localData <- 0 # 1 = yes
+doGridSpyHouseholdLoadReport <- function(){
+  source(paste0(ggrParams$repoLoc,"/makeDocs/reportTotalPower.R")) # <- requires drake. Will FAIL on CS RStudio Server 
+}
 
 # --- TUD data report ----
+
+doSurveyReport()
+#doGridSpyreport()
+#doGridSpyOutliersReport()
+#doGridSpyHouseholdLoadReport()
+
